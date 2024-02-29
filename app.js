93f28997f6676564b2938cf90a27e3dda42f418d
abdullahbalti <abdullahbalti.khan@yahoo.com>
2024-02-29 01:20:43 -0800

    fix error 404

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
 
 var app = express();
 
@@ -37,6 +41,16 @@ app.use('/blog', indexRouter);
 app.use('/users', usersRouter);
 // app.use('/', usersRouter);
 
+
+app.listen(5000, () => {
+    console.log('Server is listenin on PORT :' + PORT);
+})
+
+
+
 module.exports = app;
+
+
+
 
 // export default app;
--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ import userController from '../controller/userController';
 
 
 /* GET users listing. */
-router.get('/signup', function(req, res, next) {
-    res.json({ message: 'Testing OK' });
+router.get('/', function(req, res, next) {
+    res.json({ message: 'User Testing Route Page OK' });
   });
 router.post('/signup', userController.signup);
 router.post('/signin', userController.signin);
